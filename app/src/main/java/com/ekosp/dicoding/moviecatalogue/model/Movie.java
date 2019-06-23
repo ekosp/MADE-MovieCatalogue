036eb8e1a8faeca3e229fbf28ff742e414415261
ekosp <ekosetyopurnomo@gmail.com>
2019-06-23 22:22:54 +0700

    init commit

---
+++ app/src/main/java/com/ekosp/dicoding/moviecatalogue/model/Movie.java
@@ -0,0 +1,97 @@
+package com.ekosp.dicoding.moviecatalogue.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Eko.Purnomo on 2019-06-22.
+ * about me visit https://ekosp.com
+ * or contact me at ekosetyopurnomo@gmail.com
+ */
+
+public class Movie implements Parcelable {
+
+    private int cover;
+    private String title;
+    private String overview;
+    private Integer score;
+    private String year;
+
+    public Movie(){
+
+    };
+
+    public Movie(Parcel in) {
+        cover = in.readInt();
+        title = in.readString();
+        overview = in.readString();
+        score = in.readInt();
+        year = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(cover);
+        dest.writeString(title);
+        dest.writeString(overview);
+        dest.writeInt(score);
+        dest.writeString(year);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    public int getCover() {
+        return cover;
+    }
+
+    public void setCover(int cover) {
+        this.cover = cover;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
